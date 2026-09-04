70c046a62c3f20b51ffdff13468e363403d5d8fb
Yash darekar <dyash8608@gmail.com>
2026-09-04 22:06:36 +0530

    updating index.js

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,6 @@ app.use("/stores", storeRoutes);
 app.use("/ratings", ratingRoutes);
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port " + process.env.PORT);
 });
